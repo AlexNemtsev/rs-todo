4c5a84017da5419d3cf482b12827635ca6788d80
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2023-02-06 06:08:14 +0000

    feat: setup jest env

--- tests/components/view/clear-children.test.ts
+++ tests/components/view/clear-children.test.ts
@@ -1,10 +1,26 @@
 import { describe, expect, it, test } from '@jest/globals';
-import jsdom from 'jsdom';
 import clearChildren from '../../../src/components/view/clear-children';
 
 describe('When parent element has no children', () => {
   it("Shouldn't affect on parent", () => {
     const div = document.createElement('div');
+    clearChildren(div);
+
+    const actualNumOfChildren = div.children.length;
+    const expected = 0;
+
+    expect(actualNumOfChildren).toBe(expected);
+  });
+});
+
+describe('When parent element has children', () => {
+  it("Should remove all element's children", () => {
+    const div = document.createElement('div');
+
+    for(let i = 0; i < 5; i++) {
+      const el = document.createElement('div');
+      div.append(el);
+    }
 
     clearChildren(div);
 
