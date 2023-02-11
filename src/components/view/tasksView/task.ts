222e1e92e12a4f5dbe8c8f928a81f43344123ee3
Natalya Solovyova <ns-66@yandex.ru>
2023-02-11 13:38:39 +0700

    feat: add description field to task view

--- src/components/view/tasksView/task.ts
+++ src/components/view/tasksView/task.ts
@@ -4,13 +4,34 @@ import Utils from '../../../utils/utils';
 
 class TaskView {
   public static fillTask(item: Task): HTMLElement {
-    const { id, task, dueTo }: { id: number; task: string; dueTo?: Date | string } = item;
+    const {
+      id,
+      task,
+      dueTo,
+      desc,
+    }: {
+      id: number;
+      task: string;
+      dueTo?: Date | string;
+      desc?: string;
+    } = item;
     const taskBlock: HTMLElement = Builder.createBlock(['task']);
     taskBlock.innerHTML = `
-      <input class="task__input visually-hidden" type="checkbox" id="${id}">
-      <label class="task__label" for="${id}">${task}</label>
-      <span class="task__due-to">${Utils.convertDate(dueTo)}</span>
+      <div class="task__main">
+        <input class="task__input visually-hidden" type="checkbox" id="${id}">
+        <label class="task__label" for="${id}">${task}</label>
+        <span class="task__due-to">${Utils.convertDate(dueTo)}</span>
+      </div>
     `;
+
+    if (desc) {
+      const descBlock: HTMLElement = Builder.createBlock(
+        ['task__description'],
+        'p',
+        desc,
+      );
+      taskBlock.append(descBlock);
+    }
 
     return taskBlock;
   }
