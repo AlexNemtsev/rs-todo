b4a54ad398583d912637d2b5a63dcb497fa705f5
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-02-12 10:28:52 +0300

    feat: add new functions

--- src/components/view/tasksView/taskColumn.ts
+++ src/components/view/tasksView/taskColumn.ts
@@ -45,6 +45,8 @@ class TaskColumn {
   private fillTaskList(): void {
     Loader.getAllTasks()
       .then((taskData: Task[]) => {
+        console.log(taskData);
+
         const tasks: HTMLElement[] = taskData.map((item) =>
           TaskView.fillTask(item),
         );
