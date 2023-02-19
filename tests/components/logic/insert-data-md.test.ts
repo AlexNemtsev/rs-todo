39a074a85ff790a8cc1b3f4ddf1c7767c32cafd2
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-02-19 13:44:45 +0300

    test: change test

--- tests/components/logic/insert-data-md.test.ts
+++ tests/components/logic/insert-data-md.test.ts
@@ -1,10 +1,11 @@
-import { describe, expect, test } from '@jest/globals';
-import insertDataMd from '../../../src/components/logic/insert-data-md';
+import { expect, test } from '@jest/globals';
+import MdParser from '../../../src/components/logic/md-parser';
 
 test('Should return tagline with insertd data', () => {
-  const actual = insertDataMd('<span>A string</span>', '***Markdown***');
+  const actual = MdParser.insertDataMd('## Markdown');
 
-  const expected = '<span data-md="***Markdown***">A string</span>';
+  const expected =
+    '<h2 id="markdown" class="md md__style md__header2" data-md="## Markdown">Markdown</h2>';
 
   expect(actual).toBe(expected);
 });
