8779ab49b94506af6656aa23bb6a05cc4cbec748
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2023-02-19 19:40:15 +0300

    feat:add timer buttons and minutes input + modal fix

--- src/components/view/timer/timerView.ts
+++ src/components/view/timer/timerView.ts
@@ -11,6 +11,10 @@ class TimerView {
 
   private static radius = 0;
 
+  private static status = false;
+
+  private static animnumber = 0;
+
   private static now = () => performance.now();
 
   public static drawTimer() {
@@ -19,8 +23,16 @@ class TimerView {
      <div class="timer__wrapper">
        <div class="timer">
          <div class="timer__progress"></div>
-           <p class="timer__time">05:00</p>
+           <div class="timer__time">
+           <p class="timer__time-count">05:00</p>
+           <div class ="timer-add_window"> 
+           <div><input class="timer__input" type="number" min="0" max="180">minutes</div>
+           <div><button class="timer__cancel">cancel</button><button class="timer__ok">ok</button></div>
+           </div>
+           </div>
        </div>
+       <button class="start-stop__button">Start</button>
+       <button class="reset__button">Reset</button> 
     </div>`;
 
     const timer: Element | null = document.querySelector('.timer');
@@ -59,10 +71,11 @@ class TimerView {
   private static update(
     canvas: HTMLCanvasElement,
     context: CanvasRenderingContext2D | null,
-    time: Element | null,
+    time?: Element | null,
   ) {
     const delta = this.now() - this.startTime;
     const percent = Math.max(0, Math.min(1, delta / this.duration));
+
     const seconds = Math.round(
       (this.duration - (delta / this.duration) * this.duration) / 1000,
     );
@@ -79,7 +92,7 @@ class TimerView {
       }`;
     TimerView.draw(percent, context, canvas);
     if (delta < this.duration) {
-      requestAnimationFrame(() => {
+      this.animnumber = requestAnimationFrame(() => {
         TimerView.update(canvas, context, time);
       });
     }
@@ -88,12 +101,16 @@ class TimerView {
   private static start(
     canvas: HTMLCanvasElement,
     context: CanvasRenderingContext2D | null,
+    time: Element | null,
   ) {
-    this.startTime = this.now();
-    const time: Element | null = document.querySelector('.timer__time');
-    requestAnimationFrame(() => {
-      TimerView.update(canvas, context, time);
-    });
+    this.startTime = this.now() - this.startTime;
+    if (this.status === false) {
+      this.animnumber = requestAnimationFrame(() => {
+        TimerView.update(canvas, context, time);
+      });
+    } else {
+      cancelAnimationFrame(this.animnumber);
+    }
   }
 
   private static init(
@@ -114,10 +131,62 @@ class TimerView {
         progress.append(canvas);
       }
     }
-    if (timer)
-      timer.addEventListener('click', () => {
-        this.start(canvas, context);
-      });
+
+    const time: Element | null = document.querySelector('.timer__time-count');
+    const startButton: HTMLElement | null = document.querySelector(
+      '.start-stop__button',
+    );
+    const timerCancel: HTMLElement | null = document.querySelector(
+      '.timer__cancel',
+    );
+    const timerInput: HTMLInputElement | null = document.querySelector(
+      '.timer__input',
+    );
+    const timerOk: HTMLElement | null = document.querySelector('.timer__ok');
+    time?.addEventListener('click', () => {
+      if (timerInput) timerInput.value = (this.duration / 60000).toString();
+      if (this.status === false)
+        document.querySelector('.timer-add_window')?.classList.toggle('active');
+    });
+    timerCancel?.addEventListener('click', () => {
+      document.querySelector('.timer-add_window')?.classList.remove('active');
+    });
+    timerOk?.addEventListener('click', () => {
+      if (time && timerInput) {
+        time.innerHTML = `${
+          Math.floor(Number(timerInput.value)).toString().length === 1
+            ? `0${Math.floor(Number(timerInput.value))}`
+            : Math.floor(Number(timerInput.value))
+        }:${time.innerHTML.split(':')[1]}`;
+        this.duration = 60 * 1000 * Number(timerInput.value);
+        timerInput.value = '';
+      }
+
+      document.querySelector('.timer-add_window')?.classList.remove('active');
+    });
+    startButton?.addEventListener('click', () => {
+      this.start(canvas, context, time);
+      if (startButton)
+        startButton.innerHTML = this.status === true ? 'Start' : 'Pause';
+
+      this.status = this.status === false;
+    });
+    document.querySelector('.reset__button')?.addEventListener('click', () => {
+      this.status = true;
+      this.startTime = this.now();
+      cancelAnimationFrame(this.animnumber);
+      this.draw(0, context, canvas);
+      if (time) {
+        time.innerHTML = `${
+          (this.duration / 60000).toString().length === 1
+            ? `0${this.duration / 60000}`
+            : Math.floor(this.duration / 60000)
+        }:00`;
+      }
+      document
+        .querySelector('.start-stop__button')
+        ?.dispatchEvent(new Event('click'));
+    });
   }
 }
 
