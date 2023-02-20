8477ac299a6802ec6865dbafafbf5a583793bb78
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-02-20 13:19:42 +0300

    feat: fix non empty nested tags

--- src/components/logic/md-parser.ts
+++ src/components/logic/md-parser.ts
@@ -3,9 +3,11 @@ import i18next from 'i18next';
 import stylesMap from './styles-map';
 
 class MdParser {
+  private static emptyP = '<p id=""></p>';
+
   public static parseWithClasses(md: string): string {
     return MdParser.insertAttributes(
-      marked.parse(md) || '<p id=""></p>',
+      marked.parse(md) || MdParser.emptyP,
     ).trim();
   }
 
@@ -22,7 +24,7 @@ class MdParser {
     const indOfrt = tagLine.indexOf('>');
     const indOfSpc = tagLine.indexOf(' ');
     const tag = tagLine.substring(1, indOfSpc > indOfrt ? indOfrt : indOfSpc);
-    const emptyTag = tagLine.includes('><');
+    const emptyTag = tagLine === MdParser.emptyP;
     const dataBefore = emptyTag
       ? `data-before="${i18next.t('css.emptyBefore')}"`
       : '';
