32fdd015150af9b193d1e23c8fcf312d27608bbe
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-02-24 21:39:13 +0300

    feat: add headers saving

--- src/components/logic/handlers/on-blur-handler.ts
+++ src/components/logic/handlers/on-blur-handler.ts
@@ -1,12 +1,24 @@
 import Loader from '../loader';
 
+const isHeader = (element: Element): number => {
+  const classIdx = element.className.indexOf('md__header');
+
+  if (classIdx !== -1) return Number(element.className[classIdx + 10]);
+
+  return 0;
+};
+
 const onBlurHandler = () => {
   const details = document.querySelector('.details') as HTMLDivElement;
   const { children } = details;
   const strings: string[] = [];
 
   for (let i = 0; i < children.length; i += 1) {
-    strings.push(children[i].textContent ?? '');
+    const child = children[i];
+    const childTxt: string = child.textContent ?? '';
+    const hashSigns = '#'.repeat(isHeader(child));
+    const strToPush: string = hashSigns ? `${hashSigns} ${childTxt}` : childTxt;
+    strings.push(strToPush);
   }
 
   const taskId = Number(window.location.pathname.split('/')[3]);
