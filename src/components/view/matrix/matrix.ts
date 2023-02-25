e2e4038b3c026b437ed36b1a32ed26aa0398a69a
Natalya Solovyova <ns-66@yandex.ru>
2023-02-25 22:15:40 +0700

    feat: add loading and distribution of tasks by matrix quadrants

--- src/components/view/matrix/matrix.ts
+++ src/components/view/matrix/matrix.ts
@@ -1,15 +1,20 @@
 import i18next from 'i18next';
 import Builder from '../builder/builder';
+import Loader from '../../logic/loader';
+import TaskView from '../tasksView/task';
+import Priority from '../../../interfaces/priority';
 import './matrix.scss';
 
 class MatrixView {
+  static quadrants: HTMLElement[];
+
   public static draw(): void {
     const main = document.querySelector('main') as HTMLElement;
     main.innerHTML = '';
 
     const container: HTMLElement = Builder.createBlock(['matrix']);
-    const quadrants: HTMLElement[] = ['high', 'medium', 'low', 'none'].map(
-      (value: string) => {
+    MatrixView.quadrants = ['high', 'medium', 'low', 'none'].map(
+      (value: string, i: number, arr: string[]) => {
         const quadrant: HTMLElement = Builder.createBlock([
           'quadrant',
           `quadrant--${value}`,
@@ -20,12 +25,28 @@ class MatrixView {
           `${i18next.t(`matrix.${value}`)}`,
         );
         quadrant.append(title);
+
+        MatrixView.fillTasks(quadrant, arr.length - 1 - i);
+
         return quadrant;
       },
     );
 
-    container.append(...quadrants);
+    container.append(...MatrixView.quadrants);
     main.append(container);
+  }
+
+  public static fillTasks(quadrant: HTMLElement, priority: Priority): void {
+    Loader.getTasksByPriority(priority)
+      .then((taskData) => {
+        const tasks: HTMLElement[] = taskData.map((item) =>
+          TaskView.fillTask(item),
+        );
+        quadrant.append(...tasks);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
 }
 
