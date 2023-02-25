f0f486f7e03b3b4c0086eb41bd8930d25b0952dd
Natalya Solovyova <ns-66@yandex.ru>
2023-02-25 22:51:04 +0700

    feat: add check listener for tasks in matrix view

--- src/components/view/matrix/matrix.ts
+++ src/components/view/matrix/matrix.ts
@@ -2,51 +2,76 @@ import i18next from 'i18next';
 import Builder from '../builder/builder';
 import Loader from '../../logic/loader';
 import TaskView from '../tasksView/task';
-import Priority from '../../../interfaces/priority';
+import TaskStatus from '../../../interfaces/status';
 import './matrix.scss';
 
 class MatrixView {
-  static quadrants: HTMLElement[];
+  static quadrantTasks: HTMLElement[] = [];
 
   public static draw(): void {
     const main = document.querySelector('main') as HTMLElement;
     main.innerHTML = '';
 
     const container: HTMLElement = Builder.createBlock(['matrix']);
-    MatrixView.quadrants = ['high', 'medium', 'low', 'none'].map(
-      (value: string, i: number, arr: string[]) => {
-        const quadrant: HTMLElement = Builder.createBlock([
-          'quadrant',
-          `quadrant--${value}`,
-        ]);
-        const title: HTMLElement = Builder.createBlock(
-          ['quadrant__title'],
-          'h2',
-          `${i18next.t(`matrix.${value}`)}`,
-        );
-        quadrant.append(title);
+    const quadrants = ['high', 'medium', 'low', 'none'].map((value: string) => {
+      const quadrant: HTMLElement = Builder.createBlock([
+        'quadrant',
+        `quadrant--${value}`,
+      ]);
+      const title: HTMLElement = Builder.createBlock(
+        ['quadrant__title'],
+        'h2',
+        `${i18next.t(`matrix.${value}`)}`,
+      );
+      const taskBlock: HTMLElement = Builder.createBlock(['quadrant__tasks']);
 
-        MatrixView.fillTasks(quadrant, arr.length - 1 - i);
+      MatrixView.quadrantTasks.push(taskBlock);
+      quadrant.append(title, taskBlock);
 
-        return quadrant;
-      },
-    );
+      return quadrant;
+    });
 
-    container.append(...MatrixView.quadrants);
+    container.append(...quadrants);
     main.append(container);
+
+    MatrixView.fillTasks();
   }
 
-  public static fillTasks(quadrant: HTMLElement, priority: Priority): void {
-    Loader.getTasksByPriority(priority)
-      .then((taskData) => {
-        const tasks: HTMLElement[] = taskData.map((item) =>
-          TaskView.fillTask(item),
-        );
-        quadrant.append(...tasks);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+  public static fillTasks(): void {
+    MatrixView.quadrantTasks.forEach(
+      (quadrant, i: number, arr: HTMLElement[]) => {
+        quadrant.innerHTML = '';
+        Loader.getTasksByPriority(arr.length - 1 - i)
+          .then((taskData) => {
+            const tasks: HTMLElement[] = taskData.map((item) =>
+              TaskView.fillTask(item),
+            );
+            quadrant.append(...tasks);
+            MatrixView.addCheckListener(tasks);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+      },
+    );
+  }
+
+  private static addCheckListener(tasks: HTMLElement[]): void {
+    tasks.forEach((task) => {
+      task.addEventListener('change', (e: Event) => {
+        if (e.target instanceof HTMLInputElement) {
+          const newStatus: TaskStatus = e.target.checked ? 'done' : 'undone';
+          const taskId = Number(e.target.id);
+          Loader.updateTask(taskId, { status: newStatus })
+            .then(() => {
+              MatrixView.fillTasks();
+            })
+            .catch((error) => {
+              console.error('Error:', error);
+            });
+        }
       });
+    });
   }
 }
 
